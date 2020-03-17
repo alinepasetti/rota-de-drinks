6880a2efe300db93e0670d78879ae8603f942d9d
Gabriela Fonseca <gabmfonseca@gmail.com>
2020-03-17 18:35:25 +0000

    added loaded to the App's state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import './App.scss';
 import { Route, Switch } from 'react-router-dom';
 import HomeView from './views/Home';
@@ -20,7 +20,8 @@ class App extends Component {
   constructor() {
     super();
     this.state = {
-      user: null
+      user: null,
+      loaded: false
     };
     this.updateUserInformation = this.updateUserInformation.bind(this);
   }
@@ -29,6 +30,9 @@ class App extends Component {
     try {
       const loggedUser = await loadLoggedUserInformation();
       this.updateUserInformation(loggedUser);
+      this.setState({
+        loaded: true
+      });
     } catch (error) {
       console.log(error);
     }
@@ -43,67 +47,74 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <NavBar user={this.state.user} updateUserInformation={this.updateUserInformation} />
-        <Switch>
-          <Route path="/" exact component={HomeView} />
-          <ProtectedRoute
-            path="/sign-in"
-            exact
-            authorized={!this.state.user}
-            redirect={'/'}
-            render={props => (
-              <SignInView {...props} updateUserInformation={this.updateUserInformation} />
-            )}
-          />
-          <ProtectedRoute
-            path="/sign-up"
-            exact
-            authorized={!this.state.user}
-            redirect={'/'}
-            render={props => (
-              <SignUpView {...props} updateUserInformation={this.updateUserInformation} />
-            )}
-          />
-          <ProtectedRoute
-            path="/profile/:userId/edit"
-            exact
-            authorized={this.state.user}
-            redirect={'/sign-in'}
-            render={props => (
-              <ProfileEditView
-                {...props}
-                updateUserInformation={this.updateUserInformation}
-                user={this.state.user}
+        {this.state.loaded && (
+          <Fragment>
+            <NavBar user={this.state.user} updateUserInformation={this.updateUserInformation} />
+            <Switch>
+              <Route path="/" exact component={HomeView} />
+              <ProtectedRoute
+                path="/sign-in"
+                exact
+                authorized={!this.state.user}
+                redirect={'/'}
+                render={props => (
+                  <SignInView {...props} updateUserInformation={this.updateUserInformation} />
+                )}
               />
-            )}
-          />
-          <Route
-            path="/profile/:userId"
-            exact
-            render={props => <ProfileView user={this.state.user} {...props} />}
-          />
-          <Route path="/events" exact component={EventsListView} />
-          <Route path="/events/create" exact component={CreateEventView} />
-          <Route
-            path="/event/:eventId"
-            exact
-            render={props => <EventSingleView user={this.state.user} {...props} />}
-          />
-          {/* <ProtectedRoute
-            path="/event/:eventId/experience/intro"
-            exact
-            authorized={this.state.user}
-            redirect={'/sign-in'}
-            render={props => <ExperienceIntroView {...props} user={this.state.user} />}
-          /> */}
-          <Route path="/event/:eventId/experience/intro" exact component={ExperienceIntroView} />
-          <Route
-            path="/event/:eventId/experience/finish"
-            exact
-            render={props => <ExperienceFinishView user={this.state.user} {...props} />}
-          />
-          <Route path="/event/:eventId/experience/:stopId" exact component={ExperienceStopView} />
-        </Switch>
+              <ProtectedRoute
+                path="/sign-up"
+                exact
+                authorized={!this.state.user}
+                redirect={'/'}
+                render={props => (
+                  <SignUpView {...props} updateUserInformation={this.updateUserInformation} />
+                )}
+              />
+              <ProtectedRoute
+                path="/profile/:userId/edit"
+                exact
+                authorized={this.state.user}
+                redirect={'/sign-in'}
+                render={props => (
+                  <ProfileEditView
+                    {...props}
+                    updateUserInformation={this.updateUserInformation}
+                    user={this.state.user}
+                  />
+                )}
+              />
+              <Route
+                path="/profile/:userId"
+                exact
+                render={props => <ProfileView user={this.state.user} {...props} />}
+              />
+              <Route path="/events" exact component={EventsListView} />
+              <Route path="/events/create" exact component={CreateEventView} />
+              <ProtectedRoute
+                path="/event/:eventId/experience/intro"
+                exact
+                authorized={this.state.user}
+                redirect={'/sign-in'}
+                render={props => <ExperienceIntroView {...props} user={this.state.user} />}
+              />
+              <Route
+                path="/event/:eventId/experience/finish"
+                exact
+                render={props => <ExperienceFinishView user={this.state.user} {...props} />}
+              />
+              <Route
+                path="/event/:eventId/experience/:stopId"
+                exact
+                component={ExperienceStopView}
+              />
+              <Route
+                path="/event/:eventId"
+                exact
+                render={props => <EventSingleView user={this.state.user} {...props} />}
+              />
+            </Switch>
+          </Fragment>
+        )}
       </div>
     );
   }
